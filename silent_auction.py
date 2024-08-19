5af49e1eabf0c2a4d5a5028c554a9bf838860a86
adibkhan12 <158280738+adibkhan12@users.noreply.github.com>
2024-08-19 04:37:26 +0400

    silent_auction using dictionary

---
+++ silent_auction.py
@@ -0,0 +1,37 @@
+# TODO-1: Ask the user for input
+# TODO-2: Save data into dictionary {name: price}
+# TODO-3: Whether if new bids need to be added
+# TODO-4: Compare bids in dictionary
+import art
+print(art.logo)
+bidder_data = {}
+
+
+def find_winner():
+    highest_bid = 0
+    for key in bidder_data:
+        for bid in bidder_data:
+            if highest_bid < bidder_data[bid]:
+                highest_bid = bidder_data[bid]
+        if bidder_data[key] == highest_bid:
+            print(f"The winner is {key} with a bid of ${highest_bid}")
+
+
+def secret_auction():
+    name = input("What is your name?:")
+    price = int(input("What is your bid?: $"))
+    bidder_data[name] = price
+
+
+secret_auction()
+
+bid_over = False
+while not bid_over:
+
+    cont = input("Are there any other bidders? Type 'yes or 'no'.")
+    if cont == "yes":
+        print("\n" * 20)
+        secret_auction()
+    else:
+        find_winner()
+        bid_over = True
